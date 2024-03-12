4ae23007e71d268c044f56f77b375292d8839bb8
bkronemeyer <bkronemeyer@vmware.com>
2024-03-12 16:49:18 -0400

    remove view rest apis

--- docs/vmwb-repository/dcr-public/bca18862-8278-41ea-ba0a-de9d918fb643/d67d81a2-a3a4-4ea1-80cb-88a374e1e8b4/commonRes.js
+++
@@ -1,6 +0,0 @@
-var currentDate = new Date("2023-12");
-var currentYear = currentDate.getFullYear();
-var months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
-
-var ID_Copyright = "&copy; " + currentYear + " VMware, Inc. All rights reserved.";
-var ID_VersionInformation = "Revision " + months[currentDate.getMonth()] + "-" + currentYear + " Version 8.12.0";
